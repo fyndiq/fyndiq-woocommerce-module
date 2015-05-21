78826963d6be3691b1e794fc2bbdf4526499b29d
Håkan Nylén <hakan@fyndiq.se>
2015-05-21 14:55:08 +0200

    fixed api Helper

--- fyndiqHelper.php
+++ fyndiqHelper.php
@@ -1,4 +1,5 @@
 <?php
+require_once('api/fyndiqAPI.php');
 class FmHelpers
 {
 
@@ -21,13 +22,47 @@ class FmHelpers
      * @param array $data
      * @return mixed
      */
-    public static function callApi($storeId, $method, $path, $data = array())
+    public static function callApi($method, $path, $data = array())
     {
-        $username = FmConfig::get('username', $storeId);
-        $apiToken = FmConfig::get('apikey', $storeId);
-        $userAgent = "FyndiqMerchantMagento" . FmConfig::getVersion() . "-" . Mage::getVersion();
+        if ( ! function_exists( 'get_plugins' ) )
+            require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+        $plugin_folder = get_plugins( '/' . 'woocommerce-fyndiq' );
+        $plugin_file = 'woocommerce-fyndiq.php';
+
+        // If the plugin version number is set, return it
+        if ( isset( $plugin_folder[$plugin_file]['Version'] ) ) {
+            $plugin_version = $plugin_folder[$plugin_file]['Version'];
+        }
+        else {
+            $plugin_version = "0.0.0";
+        }
+
+        $username = get_option('wcfyndiq_username');
+        $apiToken = get_option('wcfyndiq_apitoken');
+
+        $userAgent = "FyndiqMerchantMagento" . $plugin_version . "-" . self::get_woocommerce_version();
 
         return FyndiqAPICall::callApiRaw($userAgent, $username, $apiToken, $method, $path, $data,
             array('FyndiqAPI', 'call'));
+    }
+
+    static function get_woocommerce_version() {
+        // If get_plugins() isn't available, require it
+        if ( ! function_exists( 'get_plugins' ) )
+            require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+        // Create the plugins folder and file variables
+        $plugin_folder = get_plugins( '/' . 'woocommerce' );
+        $plugin_file = 'woocommerce.php';
+
+        // If the plugin version number is set, return it
+        if ( isset( $plugin_folder[$plugin_file]['Version'] ) ) {
+            return $plugin_folder[$plugin_file]['Version'];
+
+        } else {
+            // Otherwise return null
+            return NULL;
+        }
     }
 }
