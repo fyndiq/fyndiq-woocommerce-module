f61b86b75a014964bf061383f1a22a6f6aa8e2c9
Håkan Nylén <hakan@fyndiq.se>
2015-05-26 10:24:25 +0200

    fixed more tests

--- phpunit/tests/test-fyndiq.php
+++ phpunit/tests/test-fyndiq.php
@@ -26,11 +26,26 @@ class FyndiqTest extends WP_UnitTestCase {
         $this->assertEquals($data, $this->wc_fyndiq->fyndiq_product_column_sort());
     }
 
-    function test_fyndiq_product_export_bulk_action() {
-        
+    function test_fyndiq_product_add_bulk_action() {
+        $contributor_id = $this->factory->user->create( array( 'role' => 'editor' ) );
+        wp_set_current_user( $contributor_id );
+        global $post_type;
+        $post_type = 'product';
+        //$this->assertTrue($product_id > 0);
+        //$_REQUEST['post'] = array($product_id);
+        $this->wc_fyndiq->fyndiq_product_add_bulk_action();
+        $this->expectOutputString("                    <script type=\"text/javascript\">
+                        jQuery(document).ready(function () {
+                            jQuery('<option>').val('fyndiq_export').text('Export to Fyndiq').appendTo(\"select[name='action']\");
+                            jQuery('<option>').val('fyndiq_export').text('Export to Fyndiq').appendTo(\"select[name='action2']\");
+                            jQuery('<option>').val('fyndiq_no_export').text('Remove from Fyndiq').appendTo(\"select[name='action']\");
+                            jQuery('<option>').val('fyndiq_no_export').text('Remove from Fyndiq').appendTo(\"select[name='action2']\");
+                        });
+                    </script>
+                ");
     }
 
-    private function createProduct($product) {
+    private function createProduct() {
         $post = array(
             'post_author' => 0,
             'post_content' => '',
@@ -41,7 +56,7 @@ class FyndiqTest extends WP_UnitTestCase {
 
         );
         //Create post
-        $post_id = wp_insert_post( $post, $wp_error );
+        $post_id = wp_insert_post( $post );
         wp_set_object_terms( $post_id, 'Test', 'product_cat' );
         wp_set_object_terms($post_id, 'simple', 'product_type');
 
@@ -69,21 +84,6 @@ class FyndiqTest extends WP_UnitTestCase {
         update_post_meta( $post_id, '_manage_stock', "no" );
         update_post_meta( $post_id, '_backorders', "no" );
         update_post_meta( $post_id, '_stock', "" );
-
-        // file paths will be stored in an array keyed off md5(file path)
-        $downdloadArray =array('name'=>"Test", 'file' => $uploadDIR['baseurl']."/video/".$video);
-
-        $file_path =md5($uploadDIR['baseurl']."/video/".$video);
-
-
-        $_file_paths[  $file_path  ] = $downdloadArray;
-        // grant permission to any newly added files on any existing orders for this product
-        //do_action( 'woocommerce_process_product_file_download_paths', $post_id, 0, $downdloadArray );
-        update_post_meta( $post_id, '_downloadable_files ', $_file_paths);
-        update_post_meta( $post_id, '_download_limit', '');
-        update_post_meta( $post_id, '_download_expiry', '');
-        update_post_meta( $post_id, '_download_type', '');
-        update_post_meta( $post_id, '_product_image_gallery', '');
 
         return $post_id;
     }
