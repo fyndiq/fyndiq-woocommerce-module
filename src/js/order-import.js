ddf4771c1c5fcf876c506145a387054fb2e632ab
Håkan Nylén <hakan@fyndiq.se>
2015-08-26 09:14:43 +0200

    translating javascript
    This will not comply to wordpress rules.
    They want you to use enqueue and use some weird translation library.
    That will not work with the custom buttons on orders and product pages.

--- src/js/order-import.js
+++ src/js/order-import.js
@@ -1,16 +1,16 @@
 jQuery(document).on('click', '#fyndiq-order-import', function(){
     var button = jQuery(this);
     var beforetext = button.text();
-    button.text('Loading...');
+    button.text(trans_loading);
     jQuery.ajax({
        url: wordpressurl + '/?fyndiq_orders'
    }).success(function() {
-       button.text('Done').delay(1400).queue(function(nxt) {
+       button.text(trans_done).delay(1400).queue(function(nxt) {
            jQuery(this).text(beforetext);
            location.reload();
        });
     }).fail(function() {
-        button.text('Error!').delay(1400).queue(function(nxt) {
+        button.text(trans_error).delay(1400).queue(function(nxt) {
             jQuery(this).text(beforetext);
             nxt();
         });
