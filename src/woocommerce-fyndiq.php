c403ea31ac1144a722a7710dda5e02c4fdb5a8cc
Håkan Nylén <hakan@fyndiq.se>
2015-11-10 14:48:02 +0100

    moved FmUpdate to classes

--- src/woocommerce-fyndiq.php
+++ src/woocommerce-fyndiq.php
@@ -14,12 +14,12 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
         session_start();
         require_once('include/api/fyndiqAPI.php');
         require_once('classes/FmHelpers.php');
+        require_once('classes/FmUpdate.php');
         require_once('include/shared/src/init.php');
         require_once('models/FmOrder.php');
         require_once('models/FmOrderFetch.php');
         require_once('models/FmProduct.php');
         require_once('models/FmProductFetch.php');
-        require_once('FmUpdate.php');
         require_once('WC_Fyndiq.php');
 
         $fmOuput = new FyndiqOutput();
