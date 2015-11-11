663e24c963123995aec1595340f82fb8ed9c56a5
Håkan Nylén <hakan@fyndiq.se>
2015-11-11 12:32:51 +0100

    syntax

--- src/classes/FmUpdate.php
+++ src/classes/FmUpdate.php
@@ -48,7 +48,7 @@ class FmUpdate
     {
         $response = wp_remote_get(self::UPDATE_URL);
 
-        if(isset($response['response']['code']) && 200 == $response['response']['code']) {
+        if (isset($response['response']['code']) && 200 == $response['response']['code']) {
             return json_decode($response['body']);
         }
 
