0072870de78d36a93c6cfbdbc91c74cac8dfd70e
Håkan Nylén <hakan@fyndiq.se>
2015-12-01 10:08:42 +0100

    changed license to correct one

--- src/woocommerce-fyndiq.php
+++ src/woocommerce-fyndiq.php
@@ -6,7 +6,7 @@
  * Version: 1.0.2
  * Author: Fyndiq AB
  * Author URI: http://fyndiq.se
- * License: MIT
+ * License: Commercial
  */
 
 if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
