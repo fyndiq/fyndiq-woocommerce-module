2c8659b39364c440eef71c8088add19740d90c20
Lewis Porter <lewis.porter@fyndiq.com>
2016-02-18 10:23:38 +0100

    Used WP function instead of direct server var access

--- src/classes/FmErrorHandler.php
+++ src/classes/FmErrorHandler.php
@@ -10,7 +10,7 @@ class FmError
     {
 
         add_action('admin_notices', function () {
-        
+
             if (isset($_REQUEST['fyndiqMessageType'])) {
                 echo sprintf(
                     "<div class='%s'><p>%s</p></div>",
@@ -26,7 +26,7 @@ class FmError
         $errorMessage = sprintf("An error occurred: %s", $errorMessage);
         $redirect = add_query_arg(
             array('fyndiqMessageType' => 'error', 'fyndiqMessage' => urlencode($errorMessage)),
-            $_REQUEST['_wp_http_referer']
+            wp_get_referer()
         );
         wp_redirect($redirect);
         exit;
