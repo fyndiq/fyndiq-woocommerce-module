3849a11e60f9aaaf645548c2e3251574816a7de5
Håkan Nylén <hakan@fyndiq.se>
2016-03-17 15:57:38 +0100

    indentions

--- tests/phpunit/test-fyndiq.php
+++ tests/phpunit/test-fyndiq.php
@@ -182,136 +182,135 @@ class FyndiqTest extends WP_UnitTestCase
                   'type' => 'text',
                   'default' => '10',
                   'desc' => 'Can be 0 if the price should be the same as in your shop.'),
-            array('name' => __('Global Price Discount', 'fyndiq'),
+            array('name' => 'Global Price Discount',
                   'desc_tip' => 'The amount that will be removed from the price when sending to fyndiq.',
                   'id' => 'wcfyndiq_price_discount',
                   'type' => 'text',
                   'default' => '0',
                   'desc' => 'Can be 0 if the price should not change'),
-            array('name' => __('Used Currency', 'fyndiq'),
+            array('name' => 'Used Currency',
                   'desc_tip' => 'Choose currency to be used for Fyndiq.',
-                      'id' => 'wcfyndiq_currency',
-                      'type' => 'select',
-                      'options' => array('SEK' => 'SEK', 'EUR' => 'EUR'),
-                      'desc' => 'This must be picked accurate'),
+                  'id' => 'wcfyndiq_currency',
+                  'type' => 'select',
+                  'options' => array('SEK' => 'SEK', 'EUR' => 'EUR'),
+                  'desc' => 'This must be picked accurate'),
             array('name' => 'Minimum Quantity Limit',
-                          'desc_tip' => 'this quantity will be reserved by you and will be removed from the quantity that is sent to Fyndiq.',
-                          'id' => 'wcfyndiq_quantity_minimum',
-                          'type' => 'text',
-                          'default' => '0',
-                          'desc' => 'Stay on 0 if you want to send all stock to Fyndiq.'),
-            array('name' => __('Enable Orders', 'fyndiq'),
-                              'desc_tip' => 'This will disable all order logic for Fyndiq',
-                              'id' => 'wcfyndiq_order_enable',
-                              'type' => 'select',
-                              'options' => array(
-                                  2 => 'Enable',
-                                  1 => 'Disable',
-                              ),
-                              'desc' => 'Default is to have orders enabled'),
-                              array('name' => 'Order Status',
-                              'desc_tip' => 'When a order is imported from fyndiq, this status will be applied.',
-                              'id' => 'wcfyndiq_create_order_status',
-                              'type' => 'select',
-                              'options' => array(
-                              'completed' => 'completed',
-                              'processing' => 'processing',
-                              'pending' => 'pending',
-                              'on-hold' => 'on-hold'
-                              ),
-                              'desc' => 'This must be picked accurate'),
-
+                  'desc_tip' => 'this quantity will be reserved by you and will be removed from the quantity that is sent to Fyndiq.',
+                  'id' => 'wcfyndiq_quantity_minimum',
+                  'type' => 'text',
+                  'default' => '0',
+                  'desc' => 'Stay on 0 if you want to send all stock to Fyndiq.'),
+            array('name' => 'Enable Orders',
+                  'desc_tip' => 'This will disable all order logic for Fyndiq',
+                  'id' => 'wcfyndiq_order_enable',
+                  'type' => 'select',
+                  'options' => array(
+                      2 => 'Enable',
+                      1 => 'Disable',
+                  ),
+                  'desc' => 'Default is to have orders enabled'),
+            array('name' => 'Order Status',
+                  'desc_tip' => 'When a order is imported from fyndiq, this status will be applied.',
+                  'id' => 'wcfyndiq_create_order_status',
+                  'type' => 'select',
+                  'options' => array(
+                      'completed' => 'completed',
+                      'processing' => 'processing',
+                      'pending' => 'pending',
+                      'on-hold' => 'on-hold'
+                  ),
+                  'desc' => 'This must be picked accurate'),
         );
 
 
-                    $expected[] = array(
-                    'type' => 'sectionend',
-                    'id' => 'wc_settings_wcfyndiq_section_end'
-                    );
+        $expected[] = array(
+            'type' => 'sectionend',
+            'id' => 'wc_settings_wcfyndiq_section_end'
+        );
 
-                    $expected[] = array(
-                    'name'     => __('Field Mappings', 'fyndiq'),
-                    'type'     => 'title',
-                    'desc'     => '',
-                    'id'       => 'wc_settings_wcfyndiq_section_title'
-                    );
+        $expected[] = array(
+            'name'     => 'Field Mappings',
+            'type'     => 'title',
+            'desc'     => '',
+            'id'       => 'wc_settings_wcfyndiq_section_title'
+        );
 
 
-                // Add Description picker
-                    $expected[] = array(
-                    'name' => __('Description to use', 'fyndiq'),
-                    'desc_tip' => __(
-                        'Set how you want your description to be exported to Fyndiq.',
-                        'fyndiq'
+        // Add Description picker
+        $expected[] = array(
+            'name' => 'Description to use',
+            'desc_tip' => __(
+                'Set how you want your description to be exported to Fyndiq.',
+                'fyndiq'
+            ),
+            'id' => 'wcfyndiq_description_picker',
+            'type' => 'select',
+            'options' => array(
+                FmExport::DESCRIPTION_LONG => 'Long Description',
+                FmExport::DESCRIPTION_SHORT => 'Short Description',
+                FmExport::DESCRIPTION_SHORT_LONG => 'Short and Long Description',
+            ),
+            'desc' => 'Default is Long Description',
+        );
+
+        // Map Field for EAN
+        $expected[] = array(
+            'name' => 'EAN',
+            'desc_tip' => __(
+                'EAN',
+                'fyndiq'
                     ),
-                    'id' => 'wcfyndiq_description_picker',
-                    'type' => 'select',
-                    'options' => array(
-                        FmExport::DESCRIPTION_LONG => __('Long Description', 'fyndiq'),
-                        FmExport::DESCRIPTION_SHORT => __('Short Description', 'fyndiq'),
-                        FmExport::DESCRIPTION_SHORT_LONG => __('Short and Long Description', 'fyndiq'),
-                    ),
-                    'desc' => __('Default is Long Description', 'fyndiq'),
-                    );
+            'id' => 'wcfyndiq_field_map_ean',
+            'type' => 'select',
+            'options' => array('' => ''),
+            'desc' => 'This must be picked accurate',
+        );
 
-                // Map Field for EAN
-                    $expected[] = array(
-                    'name' => __('EAN', 'fyndiq'),
-                    'desc_tip' => __(
-                        'EAN',
-                        'fyndiq'
-                    ),
-                    'id' => 'wcfyndiq_field_map_ean',
-                    'type' => 'select',
-                    'options' => array('' => ''),
-                    'desc' => __('This must be picked accurate', 'fyndiq'),
-                    );
+        // Map Field for ISBN
+        $expected[] = array(
+            'name' => 'ISBN',
+            'desc_tip' => __(
+                'ISBN',
+                'fyndiq'
+            ),
+            'id' => 'wcfyndiq_field_map_isbn',
+            'type' => 'select',
+            'options' => array('' => ''),
+            'desc' => 'This must be picked accurate',
+        );
 
-                // Map Field for ISBN
-                    $expected[] = array(
-                    'name' => __('ISBN', 'fyndiq'),
-                    'desc_tip' => __(
-                        'ISBN',
-                        'fyndiq'
-                    ),
-                    'id' => 'wcfyndiq_field_map_isbn',
-                    'type' => 'select',
-                    'options' => array('' => ''),
-                    'desc' => __('This must be picked accurate', 'fyndiq'),
-                    );
+        // Map Field for MPN
+        $expected[] = array(
+            'name' => 'MPN',
+            'desc_tip' => __(
+                'MPN',
+                'fyndiq'
+            ),
+            'id' => 'wcfyndiq_field_map_mpn',
+            'type' => 'select',
+            'options' => array('' => ''),
+            'desc' => 'This must be picked accurate',
+        );
 
-                // Map Field for MPN
-                    $expected[] = array(
-                    'name' => __('MPN', 'fyndiq'),
-                    'desc_tip' => __(
-                        'MPN',
-                        'fyndiq'
-                    ),
-                    'id' => 'wcfyndiq_field_map_mpn',
-                    'type' => 'select',
-                    'options' => array('' => ''),
-                    'desc' => __('This must be picked accurate', 'fyndiq'),
-                    );
+        // Map Field for MPN
+        $expected[] = array(
+            'name' => 'Brand',
+            'desc_tip' => __(
+                'Brand',
+                'fyndiq'
+            ),
+            'id' => 'wcfyndiq_field_map_brand',
+            'type' => 'select',
+            'options' => array('' => ''),
+            'desc' => 'This must be picked accurate',
+        );
 
-                // Map Field for MPN
-                    $expected[] = array(
-                    'name' => __('Brand', 'fyndiq'),
-                    'desc_tip' => __(
-                        'Brand',
-                        'fyndiq'
-                    ),
-                    'id' => 'wcfyndiq_field_map_brand',
-                    'type' => 'select',
-                    'options' => array('' => ''),
-                    'desc' => __('This must be picked accurate', 'fyndiq'),
-                    );
+        $expected[] = array(
+            'type' => 'sectionend',
+            'id' => 'wc_settings_wcfyndiq_section_end'
+        );
 
-                    $expected[] = array(
-                    'type' => 'sectionend',
-                    'id' => 'wc_settings_wcfyndiq_section_end'
-                    );
-
-                    $this->assertEquals($expected, $return);
+        $this->assertEquals($expected, $return);
     }
 
     function test_fyndiq_order_meta_boxes()
