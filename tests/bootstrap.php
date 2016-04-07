f14bb967d7c2bb5f9a32767338ffa50088e37699
Lewis Porter <lewis.porter@gmail.com>
2016-04-07 11:06:23 +0200

    PEAR standards

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,9 @@
 DEFINE('FYNDIQ_ROOT', './src/');
 DEFINE('ABSPATH', 1);
 
-require_once(FYNDIQ_ROOT . 'include/shared/src/init.php');
-require_once(FYNDIQ_ROOT . 'WC_Fyndiq.php');
-require_once(FYNDIQ_ROOT . 'classes/FmHelpers.php');
-require_once(FYNDIQ_ROOT . 'classes/FmUpdate.php');
-require_once(FYNDIQ_ROOT . 'classes/FmExport.php');
-require_once(FYNDIQ_ROOT . 'classes/FmError.php');
+require_once FYNDIQ_ROOT . 'include/shared/src/init.php';
+require_once FYNDIQ_ROOT . 'WC_Fyndiq.php';
+require_once FYNDIQ_ROOT . 'classes/FmHelpers.php';
+require_once FYNDIQ_ROOT . 'classes/FmUpdate.php';
+require_once FYNDIQ_ROOT . 'classes/FmExport.php';
+require_once FYNDIQ_ROOT . 'classes/FmError.php';
